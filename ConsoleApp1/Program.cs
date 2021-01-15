8ec55576582f98b04052f87aba8df111a5e8c19c
修平 山田 <31128@ojs.ac.jp>
2021-01-15 11:18:14 +0900

    ユーザ情報取得実装

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,67 +4,63 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
-using System.Net.Http;
 using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+using System.Web.Script.Serialization;
 
 namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
-            Uri webUri = new Uri("https://api.steampowered.com/ISteamApps/GetAppList/v2/");
+            
 
-            Task<string> webTask = GetWebPageAsync(webUri);
-            webTask.Wait(); // Mainメソッドではawaitできないので、処理が完了するまで待機する
-            string result = webTask.Result; 
+            //Uri webUri = new Uri("https://api.steampowered.com/ISteamApps/GetAppList/v2/");
 
-            //if (result != null) {
-            //    // サンプルとして、取得したHTMLデータの<h1>タグ以降を一定長だけ表示
-            //    Console.WriteLine("========");
-            //    int h1pos = result.IndexOf("<h1", StringComparison.OrdinalIgnoreCase);
-            //    if (h1pos < 0)
-            //        h1pos = 0;
-            //    const int MaxLength = 720;
-            //    int len = result.Length - h1pos;
-            //    if (len > MaxLength)
-            //        len = MaxLength;
-            //    Console.WriteLine(result.Substring(h1pos, len));
-            //    Console.WriteLine("========");
-            //}
-            Console.WriteLine(result);
+            //Task<string> webTask = GetWebPageAsync(webUri);
+            //webTask.Wait(); // Mainメソッドではawaitできないので、処理が完了するまで待機する
+            //string result = webTask.Result;
+            //Console.WriteLine(result);
+
+            //var wc = new WebClient();
+            //var html = wc.DownloadString("https://api.steampowered.com/ISteamApps/GetAppList/v2/");
+            //Console.WriteLine(html);
+
+
         }
-        static async Task<string> GetWebPageAsync(Uri uri) {
-            using (HttpClient client = new HttpClient()) {
-                // ユーザーエージェント文字列をセット（オプション）
-                client.DefaultRequestHeaders.Add(
-                    "User-Agent",
-                    "Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko");
+        //static async Task<string> GetWebPageAsync(Uri uri) {
+        //    using (HttpClient client = new HttpClient()) {
+        //        // ユーザーエージェント文字列をセット（オプション）
+        //        client.DefaultRequestHeaders.Add(
+        //            "User-Agent",
+        //            "Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko");
 
-                // 受け入れ言語をセット（オプション）
-                client.DefaultRequestHeaders.Add("Accept-Language", "ja-JP");
+        //        // 受け入れ言語をセット（オプション）
+        //        client.DefaultRequestHeaders.Add("Accept-Language", "ja-JP");
 
-                // タイムアウトをセット（オプション）
-                client.Timeout = TimeSpan.FromSeconds(10.0);
+        //        // タイムアウトをセット（オプション）
+        //        client.Timeout = TimeSpan.FromSeconds(10.0);
 
-                try {
-                    // Webページを取得するのは、事実上この1行だけ
-                    return await client.GetStringAsync(uri);
-                }
-                catch (HttpRequestException e) {
-                    // 404エラーや、名前解決失敗など
-                    Console.WriteLine("\n例外発生!");
-                    // InnerExceptionも含めて、再帰的に例外メッセージを表示する
-                    Exception ex = e;
-                    while (ex != null) {
-                        Console.WriteLine("例外メッセージ: {0} ", ex.Message);
-                        ex = ex.InnerException;
-                    }
-                }
-                catch (TaskCanceledException e) {
-                    // タスクがキャンセルされたとき（一般的にタイムアウト）
-                    Console.WriteLine("\nタイムアウト!");
-                    Console.WriteLine("例外メッセージ: {0} ", e.Message);
-                }
-                return null;
-            }
-        }
+        //        try {
+        //            // Webページを取得するのは、事実上この1行だけ
+        //            return await client.GetStringAsync(uri);
+        //        }
+        //        catch (HttpRequestException e) {
+        //            // 404エラーや、名前解決失敗など
+        //            Console.WriteLine("\n例外発生!");
+        //            // InnerExceptionも含めて、再帰的に例外メッセージを表示する
+        //            Exception ex = e;
+        //            while (ex != null) {
+        //                Console.WriteLine("例外メッセージ: {0} ", ex.Message);
+        //                ex = ex.InnerException;
+        //            }
+        //        }
+        //        catch (TaskCanceledException e) {
+        //            // タスクがキャンセルされたとき（一般的にタイムアウト）
+        //            Console.WriteLine("\nタイムアウト!");
+        //            Console.WriteLine("例外メッセージ: {0} ", e.Message);
+        //        }
+        //        return null;
+        //    }
+        //}
     }
 }
---
+++ Sotusei/Game.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sotusei {
+    public class GameApp {
+        [JsonProperty("@appid")]
+        public string Appid { get; set; }
+
+        [JsonProperty("@name")]
+        public string Name { get; set; }
+
+    }
+    public class UserInformation {
+        [JsonProperty("@steamid")]
+        public string Steamid { get; set; }
+
+        [JsonProperty("@name")]
+        public string Name { get; set; }
+
+        [JsonProperty("@personaname")]
+        public string Personaname { get; set; }
+
+        [JsonProperty("@avatarfull")]
+        public string Avatarfull { get; set; }
+
+
+    }
+    
+}
